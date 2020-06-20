f9b0dbbf8d4e1a61fd78d27d445776d96e3864b7
JuanCorado <juancorado1@gmail.com>
2020-06-19 18:51:13 -0700

    Issue with Clarifai api

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 
 const app = new Clarifai.App({
-  apiKey: 'a324e6296dca4e099431b285b252c73c'
+  apiKey: 'a324e6296dca4e099431b285b252c73c',
  });
 
 const particlesOption = {
@@ -40,7 +40,24 @@ class App extends Component {
       boxAll: [],
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -74,7 +91,18 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+          })
+        })
+      }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -98,15 +126,15 @@ class App extends Component {
         { route === 'home'
         ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm onInputChange={this.onInputChange} 
                           onButtonSubmit={this.onButtonSubmit}/>
             <FaceRecognition boxAll={boxAll} imageUrl={imageUrl}/>
           </div>
         : (
           route === 'signin' 
-            ? <SignIn onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange} />
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )
         }
       </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,48 @@
 import React from "react";
 
-const Register = ({ onRouteChange }) => {
-  return (
-    <div>
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    }
+  }
+
+  onNameChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value})
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/register', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+    .then(response => response.json())
+    .then(user => {
+      if(user) {
+        this.props.loadUser(user);
+        this.props.onRouteChange('home');
+      }
+    })
+  }
+
+  render() {
+    return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
@@ -17,6 +57,7 @@ const Register = ({ onRouteChange }) => {
                   type="text"
                   name="name"
                   id="name"
+                  onChange={this.onNameChange}
                 />
               </div>
               <div className="mt3">
@@ -28,6 +69,7 @@ const Register = ({ onRouteChange }) => {
                   type="email"
                   name="email-address"
                   id="email-address"
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -39,12 +81,13 @@ const Register = ({ onRouteChange }) => {
                   type="password"
                   name="password"
                   id="password"
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={() => onRouteChange("home")}
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
@@ -53,8 +96,8 @@ const Register = ({ onRouteChange }) => {
           </div>
         </main>
       </article>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Register;
